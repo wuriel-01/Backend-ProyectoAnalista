3e8516b608978c4038b5047dfb64a4b3fceb4d44
Wpxchi <urielcastillo891@gmail.com>
2026-09-12 12:04:43 -0300

    conexion a superbase

---
+++ src/config/db.js
@@ -0,0 +1,24 @@
+const { Sequelize } = require("sequelize");
+require("dotenv").config();
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    dialect: "postgres",
+
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    },
+
+    logging: false
+  }
+);
+
+module.exports = sequelize;
